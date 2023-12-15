84834c8d6c68f6cc9ae161b88c505c997ffe6c18
gigclone <153342498+gigclone@users.noreply.github.com>
2023-12-15 12:04:10 +0700

    Add files via upload

---
+++ senmenu.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+# // config Data
+echo -e "${GREEN}Config Data${NC}"
+
+cd /usr/bin/
+wget -q -O menu "https://raw.githubusercontent.com/gigclone/sc/main/menu/menu.sh"
+wget -q -O menuv "https://raw.githubusercontent.com/gigclone/sc/main/menu/menuv.sh"
+wget -q -O menul "https://raw.githubusercontent.com/gigclone/sc/main/menu/menul.sh"
+wget -q -O menut "https://raw.githubusercontent.com/gigclone/sc/main/menu/menut.sh"
+wget -q -O menussh "https://raw.githubusercontent.com/gigclone/sc/main/menu/menussh.sh"
+wget -q -O menus "https://raw.githubusercontent.com/gigclone/sc/main/menu/menus.sh"
+wget -q -O addl "https://raw.githubusercontent.com/gigclone/sc/main/menu/addl.sh"
+wget -q -O adds "https://raw.githubusercontent.com/gigclone/sc/main/menu/adds.sh"
+wget -q -O addt "https://raw.githubusercontent.com/gigclone/sc/main/menu/addt.sh"
+wget -q -O addv "https://raw.githubusercontent.com/gigclone/sc/main/menu/addv.sh"
+wget -q -O cekl "https://raw.githubusercontent.com/gigclone/sc/main/menu/cekl.sh"
+wget -q -O ceks "https://raw.githubusercontent.com/gigclone/sc/main/menu/ceks.sh"
+wget -q -O cekt "https://raw.githubusercontent.com/gigclone/sc/main/menu/cekt.sh"
+wget -q -O cekv "https://raw.githubusercontent.com/gigclone/sc/main/menu/cekv.sh"
+wget -q -O dell "https://raw.githubusercontent.com/gigclone/sc/main/menu/dell.sh"
+wget -q -O dels "https://raw.githubusercontent.com/gigclone/sc/main/menu/dels.sh"
+wget -q -O delt "https://raw.githubusercontent.com/gigclone/sc/main/menu/delt.sh"
+wget -q -O delv "https://raw.githubusercontent.com/gigclone/sc/main/menu/delv.sh"
+wget -q -O renel "https://raw.githubusercontent.com/gigclone/sc/main/menu/renel.sh"
+wget -q -O renes "https://raw.githubusercontent.com/gigclone/sc/main/menu/renes.sh"
+wget -q -O renev "https://raw.githubusercontent.com/gigclone/sc/main/menu/renev.sh"
+wget -q -O renet "https://raw.githubusercontent.com/gigclone/sc/main/menu/renet.sh"
+wget -q -O usern "https://raw.githubusercontent.com/gigclone/sc/main/menu/usern.sh"
+wget -q -O restart "https://raw.githubusercontent.com/gigclone/sc/main/restart.sh"
+wget -q -O runing "https://raw.githubusercontent.com/gigclone/sc/main/runing.sh"
+wget -q -O clearlog "https://raw.githubusercontent.com/gigclone/sc/main/clearlog.sh"
+wget -q -O genssl "https://raw.githubusercontent.com/gigclone/sc/main/genssl.sh"
+wget -q -O addhost "https://raw.githubusercontent.com/gigclone/sc/main/addhost.sh"
+
+
+chmod +x menu
+chmod +x menuv
+chmod +x menul
+chmod +x menut
+chmod +x menussh
+chmod +x menus
+chmod +x addl
+chmod +x adds
+chmod +x addt
+chmod +x addv
+chmod +x cekl
+chmod +x ceks
+chmod +x cekt
+chmod +x cekv
+chmod +x dell
+chmod +x dels
+chmod +x delt
+chmod +x delv
+chmod +x renel
+chmod +x renes
+chmod +x renev
+chmod +x renet
+chmod +x usern
+chmod +x restart
+chmod +x runing
+chmod +x clearlog
+chmod +x genssl
+chmod +x addhost
+
